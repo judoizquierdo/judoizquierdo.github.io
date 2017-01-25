516a6b42e94fefa54875c07740d445aed2443842
nachogarcia <lupus.zgz@gmail.com>
2017-01-25 13:35:00 +0100

    Use truly AJAX

--- js/contact_me.js
+++ js/contact_me.js
@@ -14,10 +14,11 @@ $(function() {
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
+            var gotcha = $("input#gotcha").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
+                firstName = name.split(' ')[0];
             }
             $.ajax({
                 url: "https://formspree.io/judoizquierdo@gmail.com",
@@ -25,11 +26,10 @@ $(function() {
                 data: {
                     name: name,
                     email: email,
+                    _subject: phone,
                     message: message,
                     _gotcha: gotcha,
-                    _subject: phone
                 },
-                cache: false;
                 datatype: "json",
                 success: function() {
                     // Success message
@@ -49,7 +49,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Lo sentimos " + firstName + ", parece que el servidor de correo no funciona. Contacte con nosotros por teléfono.");
+                    $('#success > .alert-danger').append("<strong>Lo sentimos " + firstName + ", parece que el servidor de correo no funciona. <br />Contacte con nosotros por teléfono.");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
